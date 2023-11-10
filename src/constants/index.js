4fbaea5a0ace307260efc0eddbffc7af21fed799
Omar ElDwary <omareldwary91@gmail.com>
2023-11-10 17:42:56 +0200

    final-deploy

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 import { FaFacebookF } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa6";
-import { FaXTwitter } from "react-icons/fa6";
+import { BsWhatsapp } from "react-icons/bs";
 import { AiFillLinkedin } from "react-icons/ai";
 
 export const navItems = [
@@ -23,22 +23,21 @@ export const sideNavItems = [
     },
     {
         id: "Linkedin",
-        icon: <AiFillLinkedin></AiFillLinkedin>,
+        icon: <AiFillLinkedin />,
         link: "https://www.linkedin.com/company/100225259/admin/feed/posts/?feedType=following"
     },
     {
         id: "Instagram",
-        icon: <FaInstagram></FaInstagram>,
+        icon: <FaInstagram />,
         link: "https://www.instagram.com/altabarakdevelopment?igshid=NTc4MTIwNjQ2YQ%3D%3D",
     },
     {
         id: "Youtube",
-        icon: <FaYoutube></FaYoutube>,
+        icon: <FaYoutube />,
         link: "https://www.youtube.com/channel/UCfGCJHFlr70k3WTBeyNgDSQ",
     },
     {
-        id: "Twitter",
-        icon: <FaXTwitter></FaXTwitter>,
-        link: "https://www.facebook.com/profile.php?id=61552211922534&mibextid=ZbWKwL",
+        id: "01150164444",
+        icon: <BsWhatsapp />,
     },
 ];
